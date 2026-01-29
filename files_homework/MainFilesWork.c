c00a2c22abdfaa9ad5e829d273e1c12166a66db6
Kanar Mohana <kanarmohana@gmail.com>
2026-01-29 21:59:50 +0200

    files homework

--- files_homework/MainFilesWork.c
+++ files_homework/MainFilesWork.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "fileswork.h"
 
 int main(){
     Person Persons[3];
--- files_homework/fileswork.c
+++ files_homework/fileswork.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include "fileswork.h"
 #define MAX_WORDS 100
 
 
